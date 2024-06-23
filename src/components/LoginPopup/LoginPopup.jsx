7b90dac12f4ad5803aca4dc5c1b26c3fee6846b5
lauanderson-rael <lauanderson38@gmail.com>
2024-06-23 01:03:50 -0300

    21-06

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import './LoginPopup.css';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const LoginPopup = ({ fecharPopup }) => (
   <div className="popup">
     <div className="popup-inner">
       <button className="close-button" onClick={fecharPopup}>X</button>
+      <h2>Login</h2>
       <form>
         <label>Email:</label>
         <input type="email" required />
@@ -14,8 +15,7 @@ const LoginPopup = ({ fecharPopup }) => (
         <button type="submit">ENTRAR</button>
       </form>
       <div className="signup-link">
-       {/* Ainda não tem conta? <Link to="/sign">Crie uma aqui...</Link> */}
-       Ainda não tem conta? <a to="/sign">Crie uma aqui...</a>
+       Ainda não tem conta? <Link to="/sign">Crie uma aqui...</Link>
       </div>
     </div>
   </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import { SignUp } from './Pages/SignUp/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<App />} />
+        <Route exact path='/sign' element={<SignUp />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);

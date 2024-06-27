2a0fdc83104af3c684313ecf6802fa36caa8a367
lauanderson-rael <lauanderson38@gmail.com>
2024-06-26 22:43:58 -0300

    26-06

---
+++ src/Pages/PageError404/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css'; 
+
+const PageError404 = () => {
+  return (
+    <div className="error-container">
+      <header className="cabecalho-error">
+        <h1>Erro <span>404</span></h1>
+      </header>
+      <div className="fundo">
+        <div className="error-message">
+            <div className="image-error">
+                <img width='200px' src="images/404mike.png" alt="404" />
+            </div>
+            <p>Desculpe, a página que você está procurando não foi encontrada <span>:(</span></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageError404;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignUp } from './Pages/SignUp/index.jsx';
 import App from './App.jsx';
+import PageError404 from './Pages/PageError404/index.jsx';
 
 function AppRoutes(){
     return(
         <Router>
             <Routes>
                 <Route exact path='/' element={<App/>} />
-                <Route exact path='/sign' element={<SignUp />} />
+                <Route path='/sign' element={<SignUp />} />
+                <Route path='*' element={<PageError404/>} />
             </Routes>
         </Router>
     )
